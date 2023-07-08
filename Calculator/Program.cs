e07fa2a847bc37c0d410c1f07ea222f42cbe2050
Yurii Krutsko <yuri.krutsko@gmail.com>
2023-07-08 22:17:39 +0300

    Added an infinite loop for the main program

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,78 +9,83 @@ string inputStr, inputMathOperator;
 double a, b, result;
 string[] mathOperators = { "+", "-", "*", "/" };
 
-// first number
 do
 {
-    Console.Write("Enter first number:  ");
-    inputStr = Console.ReadLine()!;
-    if (inputStr == "e")
-        return;
-
-    if (double.TryParse(inputStr, out a))
-        break;
-    
-    Console.WriteLine("\tUnable to parse '{0}'.", inputStr);
-} while (true);
-
-// operations
-do
-{
-    Console.Write("Enter math operator: ");
-    inputStr = Console.ReadLine()!;
-    if (inputStr == "e")
-        return;
-
-    if (mathOperators.Any(x => x.Equals(inputStr)))
+    do
     {
-        inputMathOperator = inputStr;
-        break;
+        // first number
+        Console.Write("Enter first number:  ");
+        inputStr = Console.ReadLine()!;
+        if (inputStr == "e")
+            return;
+
+        if (double.TryParse(inputStr, out a))
+            break;
+
+        Console.WriteLine("\tUnable to parse '{0}'.", inputStr);
+    } while (true);
+
+    // operations
+    do
+    {
+        Console.Write("Enter math operator: ");
+        inputStr = Console.ReadLine()!;
+        if (inputStr == "e")
+            return;
+
+        if (mathOperators.Any(x => x.Equals(inputStr)))
+        {
+            inputMathOperator = inputStr;
+            break;
+        }
+        Console.WriteLine("\tUnknown math operator '{0}'.", inputStr);
+    } while (true);
+
+
+    // second number
+    do
+    {
+        Console.Write("Enter second number: ");
+        inputStr = Console.ReadLine()!;
+        if (inputStr == "e")
+            return;
+
+        if (double.TryParse(inputStr, out b))
+            break;
+
+        Console.WriteLine("\tUnable to parse '{0}'.", inputStr);
+    } while (true);
+
+    Console.WriteLine();
+
+    //Result
+    var context = new Context();
+    switch (inputMathOperator)
+    {
+        case "+":
+            context.SetStrategy(new Add());
+            break;
+        case "-":
+            context.SetStrategy(new Sub());
+            break;
+        case "*":
+            context.SetStrategy(new Mult());
+            break;
+        case "/":
+            context.SetStrategy(new Div());
+            break;
     }
-    Console.WriteLine("\tUnknown math operator '{0}'.", inputStr);
+
+    try
+    {
+        result = context.DoMathOperation(a, b);
+        Console.WriteLine("Result: {0}", result);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    Console.WriteLine("---\n");
+
 } while (true);
-
-
-// second number
-do
-{
-    Console.Write("Enter second number: ");
-    inputStr = Console.ReadLine()!;
-    if (inputStr == "e")
-        return;
-
-    if (double.TryParse(inputStr, out b))
-        break;
-
-    Console.WriteLine("\tUnable to parse '{0}'.", inputStr);
-} while (true);
-
-Console.WriteLine();
-
-//Result
-var context = new Context();
-switch (inputMathOperator)
-{
-    case "+":
-        context.SetStrategy(new Add());
-        break;
-    case "-":
-        context.SetStrategy(new Sub());
-        break;
-    case "*":
-        context.SetStrategy(new Mult());
-        break;
-    case "/":
-        context.SetStrategy(new Div());
-        break;
-}
-try
-{
-    result = context.DoMathOperation(a, b);
-    Console.WriteLine("Result: {0}", result);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
-Console.ReadKey();
